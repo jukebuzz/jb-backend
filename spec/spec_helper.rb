855f5bda334144482d1f34a02bec1a6c829b435d
Sergey Ponomarev <sergey.ponomarev@saltpepper.ru>
2014-10-27 23:24:26 +0300

    Change order of specs to random

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require 'coveralls'
 Coveralls.wear!
 
 RSpec.configure do |config|
+  config.order = 'random'
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
